456d3d3e4137009e0f72068a53148f9dd8fcb302
Gsouffrance <105204398+Gsouffrance@users.noreply.github.com>
2023-06-01 15:56:57 +0800

    Update install.ps1

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -289,7 +289,7 @@ function ring_bell {
 
 function _main {
 	#if (-not $IsWindows) {
-	if (0){
+	if (not 1){
 		_abort -Msg "This install script can only execute on Windows." -Type "DeviceError"
 	}
 
